eb310804bc83b417f37c3175387372c799bba18f
Md Ratul <mdratul@Mds-MacBook-Air.local>
2026-09-12 15:57:14 +0600

    Add second part

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,4 @@
- cd ~/Desktop/DebStack-Asi
-git add .
-git commit -m "Add second part"
-git pushimport { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
